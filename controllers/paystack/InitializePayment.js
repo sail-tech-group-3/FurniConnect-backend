a519e80f7e8fd71f1447e72486bceda83bf7c007
isaac844966 <isaackeyz66@gmai.com>
2024-09-03 20:46:57 +0100

    init payment

--- controllers/paystack/InitializePayment.js
+++ controllers/paystack/InitializePayment.js
@@ -8,7 +8,7 @@ module.exports = catchAsync(async (req, res) => {
     email,
     amount: amount * 100,
     date,
-    callback_url: `${proc.env.CALLBACK_URL}`,
+    callback_url: `${process.env.CALLBACK_URL}`,
   };
 
   const response = await axios.post(
